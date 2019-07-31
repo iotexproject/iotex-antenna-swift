14e858a7d378106b7b506b09edad57c6e78071a7
ququzone <xueping.yang@gmail.com>
2019-07-31 18:54:06 +0800

    Initial Commit

---
+++ iotex-swift/iotex_swift.h
@@ -0,0 +1,19 @@
+//
+//  iotex_swift.h
+//  iotex-swift
+//
+//  Created by ququzone on 2019/7/31.
+//  Copyright © 2019 IoTeX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iotex_swift.
+FOUNDATION_EXPORT double iotex_swiftVersionNumber;
+
+//! Project version string for iotex_swift.
+FOUNDATION_EXPORT const unsigned char iotex_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iotex_swift/PublicHeader.h>
+
+
